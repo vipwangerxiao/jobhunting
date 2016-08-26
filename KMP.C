b23fd1d8159c5d10fedefcd97378728e6d687a87
vipwangerxiao <vipwangerxiao@users.noreply.github.com>
2016-08-26 19:32:18 +0800

    KMP算法
    详解

---
+++ KMP.C
@@ -0,0 +1,85 @@
+//来源: http://blog.csdn.net/v_july_v/article/details/7041827
+
+// 比较部分就是两个索引都没有到达尾部
+// 如果相等或者模式串到了-1 的索引,那么就要同时向前
+// 否则模式串索引要后退
+
+int KmpSearch(char* s, char* p)
+{
+	int i = 0;
+	int j = 0;
+	int sLen = strlen(s);
+	int pLen = strlen(p);
+	while (i < sLen && j < pLen)
+	{
+		//①如果j = -1，或者当前字符匹配成功（即S[i] == P[j]），都令i++，j++    
+		if (j == -1 || s[i] == p[j])
+		{
+			i++;
+			j++;
+		}
+		else
+		{
+			//②如果j != -1，且当前字符匹配失败（即S[i] != P[j]），则令 i 不变，j = next[j]    
+			//next[j]即为j所对应的next值      
+			j = next[j];
+		}
+	}
+	if (j == pLen)
+		return i - j;
+	else
+		return -1;
+}
+
+// 当前j要退回到k，然后判断p[j]==p[k]
+// 相等直接加1,否则要把k往回退
+
+void GetNext(char* p,int next[])
+{
+	int pLen = strlen(p);
+	next[0] = -1;
+	int k = -1;
+	int j = 0;
+	while (j < pLen - 1)
+	{
+		//p[k]表示前缀，p[j]表示后缀
+		if (k == -1 || p[j] == p[k]) 
+		{
+			++k;
+			++j;
+			next[j] = k;
+		}
+		else 
+		{
+			k = next[k];
+		}
+	}
+}
+
+//优化过后的next 数组求法
+void GetNextval(char* p, int next[])
+{
+	int pLen = strlen(p);
+	next[0] = -1;
+	int k = -1;
+	int j = 0;
+	while (j < pLen - 1)
+	{
+		//p[k]表示前缀，p[j]表示后缀  
+		if (k == -1 || p[j] == p[k])
+		{
+			++j;
+			++k;
+			//较之前next数组求法，改动在下面4行
+			if (p[j] != p[k])
+				next[j] = k;   //之前只有这一行
+			else
+				//因为不能出现p[j] = p[ next[j ]]，所以当出现时需要继续递归，k = next[k] = next[next[k]]
+				next[j] = next[k];
+		}
+		else
+		{
+			k = next[k];
+		}
+	}
+}
